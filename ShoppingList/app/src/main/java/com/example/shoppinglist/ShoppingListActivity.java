84af6a05002e301902c3b898b523e26d09919f26
Krzysztof Borowiec <kr.borowiec@gmail.com>
2019-05-02 13:59:44 +0200

    Shared preferences served

--- ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingListActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingListActivity.java
@@ -9,6 +9,7 @@ import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.collection.ArraySet;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -30,6 +31,10 @@ public class ShoppingListActivity extends AppCompatActivity {
     private Set<String> listItems;
     private Set<String> spinnerItems;
 
+    private static final String LIST_ITEMS_KEY = "LIST_ITEMS_KEY";
+    private static final String LIST_SPINNER_KEY = "LIST_SPINNER_KEY";
+    private static final String SHOPPING_LIST_KEY = "SHOPPING_LIST_KEY";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,8 +48,17 @@ public class ShoppingListActivity extends AppCompatActivity {
 //        fab.setOnClickListener();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        SharedPreferences.Editor editor = getSharedPreferences("", MODE_PRIVATE).edit();
-        editor.putString("", "Jakaś inna wiadomość");
-        editor.commit(); // UWAGA Może przywiesić wątek, można użyć .apply()
+        SharedPreferences sp = getSharedPreferences(SHOPPING_LIST_KEY, MODE_PRIVATE);
+        listItems  = sp.getStringSet(LIST_ITEMS_KEY, new ArraySet<String>());
+        spinnerItems  = sp.getStringSet(LIST_SPINNER_KEY, new ArraySet<String>());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor editor = getSharedPreferences(SHOPPING_LIST_KEY, MODE_PRIVATE).edit();
+        editor.putStringSet(LIST_ITEMS_KEY, listItems);
+        editor.putStringSet(LIST_SPINNER_KEY, spinnerItems);
+        editor.commit(); // UWAGA Może przywiesić wątek, można użyć .apply() działa w innym wątku, a nie chcemy odczytania przed zapisem
     }
 }
