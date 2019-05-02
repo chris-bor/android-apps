f69d6ba9fe1c49c8476d04fbb52fd2747e55f168
Krzysztof Borowiec <kr.borowiec@gmail.com>
2019-05-02 13:41:34 +0200

    Clean up shared preferences etc.

--- ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingListActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingListActivity.java
@@ -1,44 +1,50 @@
 package com.example.shoppinglist;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import android.view.View;
-import android.widget.Toast;
 
-import static com.example.shoppinglist.MainActivity.SP;
-import static com.example.shoppinglist.MainActivity.SP_KEY;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Set;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class ShoppingListActivity extends AppCompatActivity {
+
+    @BindView(R.id.itemName_ET)
+    EditText itemName;
+    @BindView(R.id.itemList)
+    ListView itemList;
+    @BindView(R.id.itemSpinner)
+    Spinner itemSpinner;
+
+    private Set<String> listItems;
+    private Set<String> spinnerItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_shopping_list);
+        ButterKnife.bind(this);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        fab.setOnClickListener();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.KEY);
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-
-        SharedPreferences.Editor editor = getSharedPreferences(SP, MODE_PRIVATE).edit();
-        editor.putString(SP_KEY, "Jakaś inna wiadomość");
+        SharedPreferences.Editor editor = getSharedPreferences("", MODE_PRIVATE).edit();
+        editor.putString("", "Jakaś inna wiadomość");
         editor.commit(); // UWAGA Może przywiesić wątek, można użyć .apply()
     }
-
 }
