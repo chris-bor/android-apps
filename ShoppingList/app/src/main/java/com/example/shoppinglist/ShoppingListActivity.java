3e0bef5f2f48919c80705988aa7b6ab799bc00d6
Krzysztof Borowiec <kr.borowiec@gmail.com>
2019-05-02 14:52:18 +0200

    Works fine but using R.layout.simple_list_item_1

--- ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingListActivity.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ShoppingListActivity.java
@@ -3,6 +3,7 @@ package com.example.shoppinglist;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -12,8 +13,9 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.collection.ArraySet;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import butterknife.BindView;
@@ -28,12 +30,14 @@ public class ShoppingListActivity extends AppCompatActivity {
     @BindView(R.id.itemSpinner)
     Spinner itemSpinner;
 
-    private Set<String> listItems;
+    private List<String> listItems;
     private Set<String> spinnerItems;
 
     private static final String LIST_ITEMS_KEY = "LIST_ITEMS_KEY";
     private static final String LIST_SPINNER_KEY = "LIST_SPINNER_KEY";
     private static final String SHOPPING_LIST_KEY = "SHOPPING_LIST_KEY";
+
+    private ArrayAdapter<String> listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,20 +48,36 @@ public class ShoppingListActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        listItems = new ArrayList<>();
+
         FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (itemName.getText() != null && !itemName.getText().toString().trim().isEmpty()) {
+                    listItems.add(itemName.getText().toString());
+                    itemName.setText("");
+                    listAdapter.notifyDataSetChanged();
+                }
+            }
+        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         SharedPreferences sp = getSharedPreferences(SHOPPING_LIST_KEY, MODE_PRIVATE);
-        listItems  = sp.getStringSet(LIST_ITEMS_KEY, new ArraySet<String>());
+//        listItems  = sp.getStringSet(LIST_ITEMS_KEY, new ArraySet<String>());
         spinnerItems  = sp.getStringSet(LIST_SPINNER_KEY, new ArraySet<String>());
+
+        listAdapter = new ArrayAdapter<String>(this,  android.R.layout.simple_list_item_1
+                , listItems);
+
+        itemList.setAdapter(listAdapter);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         SharedPreferences.Editor editor = getSharedPreferences(SHOPPING_LIST_KEY, MODE_PRIVATE).edit();
-        editor.putStringSet(LIST_ITEMS_KEY, listItems);
+//        editor.putStringSet(LIST_ITEMS_KEY, listItems);
         editor.putStringSet(LIST_SPINNER_KEY, spinnerItems);
         editor.commit(); // UWAGA Może przywiesić wątek, można użyć .apply() działa w innym wątku, a nie chcemy odczytania przed zapisem
     }
